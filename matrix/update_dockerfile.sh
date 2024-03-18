1d787cc6591beacbb4d3b6e0e12d71d1228c2485
Enrique Belarte Luque <ebelarte@redhat.com>
2024-03-18 07:44:22 -0500

    Fix update Dockerfile

--- matrix/update_dockerfile.sh
+++ matrix/update_dockerfile.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-
+set -x
 # Function to update Dockerfile with version number
 update_dockerfile() {
     json_file=$1
     dockerfile=$2
 
-    while IFS= read -r line; do
-        release_version=$(echo "$line" | jq -r '.[].release_version')
-        published=$(echo "$line" | jq -r '.[].published')
-        kernel_version=$(echo "$line" | jq -r '.[].kernel_version')
-        
-        if [ "$published" = "n" ]; then
-            sed -i "s|ARG KERNEL_VERSION='[^']*'|ARG KERNEL_VERSION='${kernel_version}'|g; s|registry\.distributed-ci\.io/dtk/driver-toolkit|FROM registry.distributed-ci.io/dtk/driver-toolkit:\${kernel_version} as builder|g" "$dockerfile"
-            sed -i "s|ARG DRIVER_VERSION='[^']*'|ARG DRIVER_VERSION='${release_version}'|g" "$dockerfile"
-        fi
-    done < "$json_file"
+    result=$(cat "$json_file" | jq -r '.[] | select(.published == "n") | {release_version, kernel_version}')
+    # Extract 'release_version' and 'kernel_version' from the result (first match)
+    release_version=$(echo "$result" | jq -r '.release_version'| head -n1)
+    kernel_version=$(echo "$result" | jq -r '.kernel_version' | head -n1 )
+    sed -i "s|ARG KERNEL_VERSION='[^']*'|ARG KERNEL_VERSION='${kernel_version}'|g" "$dockerfile"
+    sed -i "s|.*FROM registry\.distributed-ci\.io/dtk/driver-toolkit.*|FROM registry.distributed-ci.io/dtk/driver-toolkit:${kernel_version} as builder|g" "$dockerfile"
+    sed -i "s|ARG DRIVER_VERSION='[^']*'|ARG DRIVER_VERSION='${release_version}'|g" "$dockerfile"
 }
 
 # Run the function

e4ee046a4981ffda9a957c553091c534b3ace69b
Enrique Belarte Luque <ebelarte@redhat.com>
2024-03-18 07:23:24 -0500

    Update Dockerfile script

--- matrix/update_dockerfile.sh
+++ matrix/update_dockerfile.sh
@@ -6,13 +6,12 @@ update_dockerfile() {
     dockerfile=$2
 
     while IFS= read -r line; do
-        release_version=$(jq -r '.release_version' <<< "$line")
-        published=$(jq -r '.published' <<< "$line")
-        kernel_version=$(jq -r '.kernel_version' <<< "$line")
+        release_version=$(echo "$line" | jq -r '.[].release_version')
+        published=$(echo "$line" | jq -r '.[].published')
+        kernel_version=$(echo "$line" | jq -r '.[].kernel_version')
         
         if [ "$published" = "n" ]; then
             sed -i "s|ARG KERNEL_VERSION='[^']*'|ARG KERNEL_VERSION='${kernel_version}'|g; s|registry\.distributed-ci\.io/dtk/driver-toolkit|FROM registry.distributed-ci.io/dtk/driver-toolkit:\${kernel_version} as builder|g" "$dockerfile"
-
             sed -i "s|ARG DRIVER_VERSION='[^']*'|ARG DRIVER_VERSION='${release_version}'|g" "$dockerfile"
         fi
     done < "$json_file"
